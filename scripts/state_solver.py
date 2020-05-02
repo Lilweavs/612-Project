5347c055792bd9126255a5865618f3ea76daa372
Lilweavs <andrewjw827@gmail.com>
2020-05-02 14:38:01 -0500

    new stuff

--- scripts/state_solver.py
+++ scripts/state_solver.py
@@ -9,23 +9,28 @@ from robot_simulation.msg import ReleaseStates
 class StateSolver():
 
   def __init__(self):
-    self.L1 = 0.5
-    self.L2 = 0.5
-    self.q1 = 0
+    rospy.init_node("State_Solver")
+    self.L1 = 0.3
+    self.L2 = 0.3
+    self.q1 = np.pi/8
     self.q2 = np.pi/8
-    self.q3 = np.pi/8
     self.J = np.array(0)
     self.msg = ReleaseStates()
-    self.v = np.array([1, 0, -1])
-    self.pub = rospy.Publisher('/end_states', ReleaseStates, queue_size=1)
+    self.v = np.array([2, 2])
+    self.pub = rospy.Publisher("/end_states", ReleaseStates, queue_size=10)
 
   # output: q1, q2, q3, v
-  def state_solver(self, x, y, z, angle):
-    '''
-    self.msg.q1 = 0
-    self.msg.q2 = 0
-    self.msg.q3 = 0
-    '''
+  def state_solver(self, x, y, z):
+    
+    x = float(x)
+    y = float(y)
+    z = float(z)
+
+    self.msg.angle = np.arctan( y / x )
+
+    self.msg.q1 = self.q1
+    self.msg.q2 = self.q2
+
     self.Jacobian()
 
   # Jacobian of manipulators
@@ -33,33 +38,44 @@ class StateSolver():
     
     L1 = self.L1
     L2 = self.L2
-    q1 = self.q1
-    q2 = self.q2
-    q3 = self.q3
+    q1 = self.msg.q1
+    q2 = self.msg.q2
 
-    self.J = np.array(([-(L1*np.cos(q2) + L2*np.cos(q2+q3))*np.sin(q1), -(L1*np.sin(q2) + L2*np.sin(q2+q3))*np.cos(q1), -L2*np.sin(q2+q3)*np.cos(q1)],
-                  [(L1*np.cos(q2) + L2*np.cos(q2+q3))*np.cos(q1), -(L1*np.sin(q2) + L2*np.sin(q2+q3))*np.sin(q1), -L2*np.sin(q2+q3)*np.sin(q1)],
-                  [0, L1*np.cos(q2) + L2*np.cos(q2+q3), L2*np.cos(q2+q3)]))
+    # self.J = np.array(([-(L1*np.cos(q1) + L2*np.cos(q1+q2)), -(L1*np.sin(q1) + L2*np.sin(q1+q2)), -L2*np.sin(q1+q2)],
+    #               [(L1*np.cos(q1) + L2*np.cos(q1+q2)), -(L1*np.sin(q1) + L2*np.sin(q1+q2)), -L2*np.sin(q1+q2)],
+    #               [0, L1*np.cos(q1) + L2*np.cos(q1+q2), L2*np.cos(q1+q2)]))
+
+    self.J = np.array([[L1*np.cos(q1) + L2*np.cos(q1 + q2), L2*np.cos(q1 + q2)],
+                       [L1*np.sin(q1) + L2*np.sin(q1 + q2), L2*np.sin(q1 + q2)]])
+
     self.q_dot()
 
   # output: q1_dot, q2_dot, q3_dot
   # input: q1, q2, q3, vx, vy, vz, J
   def q_dot(self):
   
-    q_dot = np.linalg.solve(self.J, self.v)/(2*np.pi)*60
+    q_dot = np.linalg.solve(self.J, self.v)
 
     self.msg.q1_dot = q_dot[0]
     self.msg.q2_dot = q_dot[1]
-    self.msg.q3_dot = q_dot[2]
+    # self.msg.q3_dot = q_dot[2]
 
-    rospy.loginfo(self.msg)
     self.pub.publish(self.msg)
+    rospy.loginfo(self.msg)
   
 if __name__=="__main__":
     if (sys.argv < 5):  
-        print('usage: my_node x y z theta_t')
+        print('usage: my_node x y z')
     else:
-      rospy.init_node("Blain_Likes_Feet")
       n = StateSolver()
-      n.state_solver(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+      rate = rospy.Rate(5)
+      while not rospy.is_shutdown():
+        connections = n.pub.get_num_connections()
+        rospy.loginfo('Connections: %d', connections)
+        if connections > 0:
+          n.state_solver(sys.argv[1], sys.argv[2], sys.argv[3])
+          rospy.loginfo('Published')
+          break
+        rate.sleep()
+
         
--- scripts/trajectory_plan.py
+++ scripts/trajectory_plan.py
@@ -15,56 +15,78 @@ class Trajectory_Planner:
         self.a1 = 0
         self.a2 = 1.398
         self.a3 = -0.58528
+        self.tf = 1
         self.t0 = rospy.Time.to_sec(rospy.get_rostime())
-        self.t1 = rospy.get_rostime()
+        self.states = ReleaseStates()
+        self.coeff = []
+        self.timer = None
         self.pub_joint1 = rospy.Publisher('/simple_model/base_to_first_joint_position_controller/command', Float64, queue_size=10)
         self.pub_joint2 = rospy.Publisher('/simple_model/first_to_second_joint_position_controller/command', Float64, queue_size=10)
         self.pub_latch = rospy.Publisher('/simple_model/end_effector_to_latch_position_controller/command', Float64, queue_size=10)
         self.sub = rospy.Subscriber('/simple_model/joint_states', JointState, self.latch_release )
-        self.timer = rospy.Timer(rospy.Duration(0.1), self.publish_path)
+        # self.timer = rospy.Timer(rospy.Duration(0.1), self.publish_path)
         #self.sub = rospy.Subscriber('/clock', Clock, self.get_time_callback)
         #rospy.Timer(rospy.Duration(0.1), publish_path)
 
     def latch_release(self, msg):
-        rospy.loginfo(msg.position[0])
+        rospy.loginfo(msg.position[3])
+        if (msg.position[1]) > np.pi/16:
+            self.pub_latch.publish(np.pi/2)
 
-    def get_inital_time(self):
-        self.t0 = rospy.get_rostime()
+    def cubic(self, t0, tf):
+        retVal = np.array([[1, t0, t0**2, t0**3],
+                  [0, 1, 2*t0, 3*t0**2],
+                  [1, tf, tf**2, tf**3],
+                  [0, 1, 2*tf, 3*tf**2]])
+        return retVal
+
+    def find_trajectory_plan(self):
+        # Use jacobian to find joint velocities
+        B1 = np.array([0, 0, self.states.q1, self.states.q1_dot])
+        B2 = np.array([0, 0, self.states.q2, self.states.q2_dot])
+        A = self.cubic(0, self.tf)
+
+        retVal = [np.linalg.pinv(A).dot(B1), np.linalg.pinv(A).dot(B2)]
+    
+        return retVal
 
     def wait(self):
-        pass
-        #print('Waiting for command')
-        #rospy.wait_for_message('/run', Float64)
-        #timer = rospy.Timer(rospy.Duration(0.1), self.publish_path)
+        print('Waiting for command...')
+        self.states = rospy.wait_for_message('/end_states', ReleaseStates)
+        print('End States Received!')
+        rospy.loginfo(self.states)
+        self.coeff = self.find_trajectory_plan()
+        print('Yeet!!!')
+
+        self.t0 = rospy.Time.to_sec(rospy.get_rostime())
+        self.timer = rospy.Timer(rospy.Duration(0.1), self.publish_path)
         #self.t0 = rospy.Time.to_sec(rospy.get_rostime())
         #def publish_path(msg):
         #    print(msg)
 
-
-    def get_time_callback(self, msg):
-        pass
-        #rospy.loginfo(msg.nsec)
-        #rospy.loginfo(self.t0)
-
-
     def publish_path(self, msg):
 
-        elapsed_time = rospy.Time.to_sec(msg.current_real) - self.t0
-        val = self.a0 + self.a1*elapsed_time + self.a2*elapsed_time**2 + self.a3*elapsed_time**3
-        print('t = '+str(elapsed_time)+' q = '+str(val))
-        self.pub.publish(val)
-        if elapsed_time > 2:
-            print('shutting down')
+        t = rospy.Time.to_sec(msg.current_real) - self.t0
+        q1d = self.coeff[0][0] + self.coeff[0][1]*t + self.coeff[0][2]*t**2 + self.coeff[0][3]*t**3
+        q2d = self.coeff[1][0] + self.coeff[1][1]*t + self.coeff[1][2]*t**2 + self.coeff[1][3]*t**3
+        print('t = ' + str(t) + ' q1d = ' + str(q1d) + ', q2d = ' + str(q2d))
+        self.pub_joint1.publish(q1d)
+        self.pub_joint2.publish(q2d)
+        if t >= self.tf:
+            print('Trajectory Complete')
             self.timer.shutdown()
-
-
-def talker():
-    pub = rospy.Publisher('/simple_model/base_to_firs_link_joint_state_publisher/command', Float64) 
+        
+    def cleanup(self):
+        print('Reseting Robot Arm')
+        self.pub_joint1.publish(0)
+        self.pub_joint2.publish(0)
+        self.pub_latch.publish(0)
 
 
 if __name__=="__main__":
     rospy.init_node('trajectory_planner')
     rospy.sleep(1)
     n = Trajectory_Planner()
+    rospy.on_shutdown(n.cleanup)
     n.wait()
     rospy.spin()
